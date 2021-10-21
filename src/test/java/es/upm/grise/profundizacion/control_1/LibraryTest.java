911e013452f2d0945ad10ca1dbcd6f6de77ae2f3
Maria Gutierrez Martin <maria_ventas98@hotmail.es>
2021-10-21 18:14:10 +0200

    add unit tests for Library class

--- src/test/java/es/upm/grise/profundizacion/control_1/LibraryTest.java
+++ src/test/java/es/upm/grise/profundizacion/control_1/LibraryTest.java
@@ -1,10 +1,69 @@
 package es.upm.grise.profundizacion.control_1;
 
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions.*;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class LibraryTest {
 	
 	@Test
-	public void test() {
-		fail();
+	public void givenAValidAndNotDuplicatedBookInLibrary_whenAddBook_thenAValidBoodIsAddedToLibrary() throws DuplicatedBookException, NonExistingBookException, EmptyLibraryException {
+		Book libroValido = new Book("Don Quijote");
+		Library libreria = new Library();
+		libreria.addBook(libroValido);
+		assertEquals(libroValido.getTitle(), libreria.getBook("Don Quijote").getTitle());
 	}
+
+	@Test
+	public void givenADuplicatedBookInLibrary_whenAddBook_thenThrowsException() throws DuplicatedBookException {
+		Book libroValido = new Book("Don Quijote");
+		Book libroNoValido = new Book("Don Quijote");
+		Library libreria = new Library();
+		libreria.addBook(libroValido);
+		assertThrows(DuplicatedBookException.class, () -> libreria.addBook(libroNoValido));
+	}
+
+	@Test
+	public void givenANotExistingBookInLibrary_whenGetBook_thenThrowsException() throws DuplicatedBookException {
+		Book libroValido = new Book("Don Quijote");
+		Library libreria = new Library();
+		libreria.addBook(libroValido);
+		assertThrows(NonExistingBookException.class, () -> libreria.getBook("Hamlet"));
+	}
+
+	@Test
+	public void givenEmptyLibrary_whenGetBook_thenThrowsException(){
+		Library libreria = new Library();
+		assertThrows(EmptyLibraryException.class, () -> libreria.getBook("Hamlet"));
+	}
+
+	@Test
+	public void givenAValidAndNotDuplicatedBookInLibrary_whenGetBook_thenReturnBook() throws DuplicatedBookException, NonExistingBookException, EmptyLibraryException {
+		Book libroValido = new Book("Don Quijote");
+		Library libreria = new Library();
+		libreria.addBook(libroValido);
+		assertEquals(libroValido.getTitle(), libreria.getBook("Don Quijote").getTitle());
+	}
+
+	@Test
+	public void givenAnExistingBookInLibrary_whenRemoveBook_thenRemoveBook() throws DuplicatedBookException {
+		Book libroValido = new Book("Don Quijote");
+		Book libroValido2 = new Book("Hamlet");
+		Library libreria = new Library();
+		Library libreriaVacia = new Library();
+		libreria.addBook(libroValido);
+		libreria.addBook(libroValido2);
+		libreria.removeBook(libroValido);
+		assertThrows(NonExistingBookException.class, () -> libreria.getBook("Don Quijote"));
+	}
+
+
+
+
 
 }
